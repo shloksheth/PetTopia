a4710adc3de644e42fc5bc1fc168977fb451f93d
sb26shlokk <shlok.sheth62@gmail.com>
2025-11-20 00:27:52 -0500

    basic

--- scripts/systems/Global.gd
+++ scripts/systems/Global.gd
@@ -1,11 +1,6 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var hunger = 50
+var happiness = 50
+var energy = 50
+var coins = 10
